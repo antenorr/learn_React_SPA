4d73dc630e51f50e9325a900f58ab4d9305e1a37
antenorr <roudyantenor@gmail.com>
2018-09-03 12:47:27 -0400

    initial commit  - again completed the entir tutt before submitting the commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render () {
+        return (
+            <div>
+                <h2>Have any questions..</h2>
+                <p>The best wat to contact us is on twitter:</p>
+                <a href="http://www.twitter.com"> Twitter</a>
+            </div>
+        );
+    }
+}
+export default Contact;
---
+++ src/Stuff.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>  
+                <h2>Stuff</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <ol>
+                    <li>Cool Beans</li>
+                    <li>Futurama</li>
+                    <li>Fast Money</li>
+                    <li>Eager Beaver</li>
+                </ol>
+            </div>
+        )
+    }
+}
+
+export default Stuff;
